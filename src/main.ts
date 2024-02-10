b89dad3817a163e97dbe30d561a42e8dfe0a9d25
mgruizQA <mariagarciaruiz1990@gmail.com>
2024-02-10 14:27:31 +0100

    Commit calculo por comensal

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+/*el ticket total son 120€ por lo que quitaremos los 18€ que vamos a pagar de bebida.
+De ese total dividiremos entre 6 que es la cantidad de amigos que han ido a la cena.*/
+console.log("Cada persona pagará " + (120 - 18) / 6 + " Euros");
